ffe03fad98f8e2d87d93d4cfd3ef7b1631b16573
Rajput616 <saurabhrajput616@gmail.com>
2022-04-17 17:16:49 +0530

    First Module - Problem 3 added

--- src/Solution.java
+++ src/Solution.java
@@ -3,14 +3,39 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        String s = scn.next();
-        int len = s.length();
-        StringBuilder ans = new StringBuilder();
-        for(int i = 0; i < s.length(); ++i){
-            int digit = s.charAt(i) - '0';
-            if((i == 0 && digit == 9) || digit < 5) ans.append(digit);
-            else ans.append(9 - digit);
+        int[][] matrix = Input.matrix(5,5);
+        int i = 0, j = 0;
+        boolean isOneFound = false;
+        for(; i < 5; ++i){
+            for(j = 0; j < 5; ++j){
+                if(matrix[i][j] == 1) {
+                    isOneFound = true;
+                    break;
+                }
+            }
+            if(isOneFound) break;
         }
-        System.out.println(ans);
+        int maxMoves = Math.abs(2 - i) + Math.abs(2 - j);
+        System.out.println(maxMoves);
+    }
+}
+
+class Input{
+    public static int[] array(int size){
+        Scanner scn = new Scanner(System.in);
+        int[] arr = new int[size];
+        for(int i = 0; i < size; ++i) arr[i] = scn.nextInt();
+        return arr;
+    }
+
+    public static int[][] matrix(int rows, int cols){
+        Scanner scn = new Scanner(System.in);
+        int[][] matrix = new int[rows][cols];
+        for(int i = 0; i < rows; ++i){
+            for(int j = 0; j < cols; ++j){
+                matrix[i][j] = scn.nextInt();
+            }
+        }
+        return matrix;
     }
 }
